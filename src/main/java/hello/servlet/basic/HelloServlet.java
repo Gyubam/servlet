e3474d9f31567106768ea0873659aa99a81cb265
서규범 <sgb8170@naver.com>
2022-08-03 19:14:53 +0900

    템플릿 추가

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -20,6 +20,6 @@ public class HelloServlet extends HttpServlet {
 
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello" + username);
+        response.getWriter().write("hello " + username);
     }
 }
